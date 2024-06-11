ada1066c324b1fa26eb28ec94baecb1e8c63c1f9
Ahmad Bahr <ahmad.bahr@mubs.edu.lb>
2024-06-11 15:12:00 +0300

    adding nodemon

--- server/server.js
+++ server/server.js
@@ -2,7 +2,11 @@ require("dotenv") .config();
 
 const express = require("express")
 
-const app = express()
+const app = express();
+
+app.get("/getRestaurants", (req, res) => {
+    console.log("get all restaurants");
+})
 
 const port = process.env.PORT || 3001
 app.listen(3000, () => {
