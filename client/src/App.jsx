c3b0f400b83e86fdad8f5815605b8536ee0d37ab
Ahmad Bahr <ahmad.bahr@mubs.edu.lb>
2024-06-13 02:37:08 +0300

    routes set

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,21 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./routes/Home"
+import RestaurantsDetailPage from "./routes/RestaurantDetailPage";
+import UpdatePage from "./routes/UpdatePage"
 
 function App() {
-    return (
-        <div>
-            <h1>Hello, World!</h1>
-        </div>
-    );
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home/>} />
+          <Route exact path="/restaurants/:id/update" element={<UpdatePage />} />
+          <Route exact path="/restaurants/:id" element={<RestaurantsDetailPage />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
