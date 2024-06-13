d4e776c9952fe03c84292fbafc8a2801b093f0b4
Ahmad Bahr <ahmad.bahr@mubs.edu.lb>
2024-06-13 16:44:04 +0300

    wrapping everything in context provider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,23 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./routes/Home"
+import Home from "./routes/Home";
 import RestaurantsDetailPage from "./routes/RestaurantDetailPage";
-import UpdatePage from "./routes/UpdatePage"
+import UpdatePage from "./routes/UpdatePage";
+import { RestaurantsContextProvider } from "./context/RestaurantsContext";
 
 function App() {
   return (
-    <div className="container">
-      <Router>
-        <Routes>
-          <Route exact path="/" element={<Home/>} />
-          <Route exact path="/restaurants/:id/update" element={<UpdatePage />} />
-          <Route exact path="/restaurants/:id" element={<RestaurantsDetailPage />} />
-        </Routes>
-      </Router>
-    </div>
+    <RestaurantsContextProvider>
+      <div className="container">
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/restaurants/:id/update" element={<UpdatePage />} />
+            <Route path="/restaurants/:id" element={<RestaurantsDetailPage />} />
+          </Routes>
+        </Router>
+      </div>
+    </RestaurantsContextProvider>
   );
 }
 
