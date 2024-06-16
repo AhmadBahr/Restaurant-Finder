10e50456ed19b3a06cc6f7f34cc6025bf4d9a9b1
Ahmad Bahr <ahmad.bahr@mubs.edu.lb>
2024-06-16 21:35:05 +0300

    adding restaurant

--- client/src/components/AddRestaurant.jsx
+++ client/src/components/AddRestaurant.jsx
@@ -1,31 +1,53 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const AddRestaurant = () => {
-  return (
-    <div className='mb-4'>
-        <form action=''>
-        <div className="form-row" style={{display:'flex'}}>
-                <div className='col'>
-                    <input type="text" className='form-control' placeholder='Name'/>
-                </div>
-                <div className='col'>
-                    <input type="text" className='form-control' placeholder='Location'/>
-                </div>
-                <div className="col">
-                    <select className="custom-select my - 1 mr - sm - 2">
-                        <option disabled>Price Range</option>
-                        <option value="1">$</option>
-                        <option value="2">$$</option>
-                        <option value="3">$$$</option>
-                        <option value="4">$$$$</option>
-                        <option value="5">$$$$$</option>
-                    </select>
-                    <button className='btn btn-primary'>Add</button>
-                </div>
-            </div>
-        </form>
-    </div>
-  )
-}
+    const [name, setName] = useState("");
+    const [location, setLocation] = useState("");
+    const [priceRange, setPriceRange] = useState("Price Range");
 
-export default AddRestaurant
+    return (
+        <div className="mb-4">
+            <form action="">
+                <div className="form-row" style={{ display: "flex" }}>
+                    <div className="col">
+                        <input
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                            type="text"
+                            className="form-control"
+                            placeholder="Name"
+                        />
+                    </div>
+                    <div className="col">
+                        <input
+                            value={location}
+                            onChange={(e) => setLocation(e.target.value)}
+                            type="text"
+                            className="form-control"
+                            placeholder="Location"
+                        />
+                    </div>
+                    <div className="col">
+                        <select
+                            value={priceRange}
+                            onChange={(e) => setPriceRange(e.target.value)}
+                            className="custom-select my-1 mr-sm-2"
+                        >
+                            <option disabled>Price Range</option>
+                            <option value="1">$</option>
+                            <option value="2">$$</option>
+                            <option value="3">$$$</option>
+                            <option value="4">$$$$</option>
+                            <option value="5">$$$$$</option>
+                        </select>
+                    </div>
+                    <div className="col">
+                        <button className="btn btn-primary">Add</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    );
+};
+
+export default AddRestaurant;
