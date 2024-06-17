71aa08c4b80a0976f643ba12fd17a16448413b86
Ahmad Bahr <ahmad.bahr@mubs.edu.lb>
2024-06-18 00:23:55 +0300

    restaurant detail page

--- client/src/routes/RestaurantDetailPage.jsx
+++ client/src/routes/RestaurantDetailPage.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { useContext, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import RestaurantFinder from "../APIs/RestaurantFinder";
+import { RestaurantsContext } from "../context/RestaurantsContext";
 
 const RestaurantDetailPage = () => {
+  const { id } = useParams();
+  const { selectedRestaurant, setSelectedRestaurant } =
+    useContext(RestaurantsContext);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await RestaurantFinder.get(`/${id}`);
+        setSelectedRestaurant(response.data.restaurant);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    fetchData();
+  }, [id, setSelectedRestaurant]);
+
+  if (!selectedRestaurant) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
-      
+      <h1>{selectedRestaurant.name}</h1>
+      <p>Location: {selectedRestaurant.location}</p>
+      <p>Price Range: {"$".repeat(selectedRestaurant.price_range)}</p>
+      <p>Phone: {selectedRestaurant.phone}</p>
+      <p>Email: {selectedRestaurant.email}</p>
     </div>
-  )
-}
+  );
+};
 
-export default RestaurantDetailPage
+export default RestaurantDetailPage;
